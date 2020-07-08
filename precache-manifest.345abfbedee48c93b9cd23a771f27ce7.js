449dac95a7152b6cb643738d75defaeeff62a5ba
keloriane <kevin.flbt@gmail.com>
2020-07-08 17:00:41 +0200

    Updates

---
+++ precache-manifest.345abfbedee48c93b9cd23a771f27ce7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9743b18a45f2c36df8d32e07605ed733",
+    "url": "/debriefs/index.html"
+  },
+  {
+    "revision": "1da047dfd44fdaa8660a",
+    "url": "/debriefs/static/css/main.8969c9c9.chunk.css"
+  },
+  {
+    "revision": "df305d00902058c690a5",
+    "url": "/debriefs/static/js/2.8933b10b.chunk.js"
+  },
+  {
+    "revision": "8e1f2a45f25560b4c98213b97585f69d",
+    "url": "/debriefs/static/js/2.8933b10b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1da047dfd44fdaa8660a",
+    "url": "/debriefs/static/js/main.02c2cba5.chunk.js"
+  },
+  {
+    "revision": "46be99811cf2d5924e25",
+    "url": "/debriefs/static/js/runtime-main.62e649a7.js"
+  },
+  {
+    "revision": "ac8ebf27cbc1e133a77cf28e23924fc2",
+    "url": "/debriefs/static/media/articles.ac8ebf27.png"
+  },
+  {
+    "revision": "e6c4965994cf996582fef755482104af",
+    "url": "/debriefs/static/media/home.e6c49659.png"
+  },
+  {
+    "revision": "592ee1626bd0ee9a264f59c056e3b606",
+    "url": "/debriefs/static/media/team.592ee162.png"
+  },
+  {
+    "revision": "4031a755e062c9309541da55417a2176",
+    "url": "/debriefs/static/media/webinaires.4031a755.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/debriefs/precache-manifest.206339df04539b94dd5343de449ee1ff.js"
+  "/debriefs/precache-manifest.345abfbedee48c93b9cd23a771f27ce7.js"
 );
 
 self.addEventListener('message', (event) => {
